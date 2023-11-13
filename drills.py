f98fd05e3a829bfedfcce993efd154f2db6e0e6f
agblituania <ablituania@up.edu.ph>
2023-11-13 12:04:43 +0800

    fourth step dummy

--- drills.py
+++ drills.py
@@ -5,3 +5,4 @@ import random as r
 df = pd.read_csv("table1.csv").dropna()                                             #import
 df.insert(0, "ID" , [i for i in range(len(df.index))])                              #add ID
 df["Date"] = "META-" + df["Date"]                                                   #modify date
+df.insert(2, "Dummy" , [r.randint(0,1) for _ in range(len(df.index))])              #dummy
