35f1ac100e654363ee8d4553c279047931d81cb0
agblituania <ablituania@up.edu.ph>
2023-11-13 12:03:19 +0800

    third step modify date

--- drills.py
+++ drills.py
@@ -4,3 +4,4 @@ import random as r
 
 df = pd.read_csv("table1.csv").dropna()                                             #import
 df.insert(0, "ID" , [i for i in range(len(df.index))])                              #add ID
+df["Date"] = "META-" + df["Date"]                                                   #modify date
