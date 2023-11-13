df782cf5a062cc1694b5d6e2dc8e430d04e465bd
agblituania <ablituania@up.edu.ph>
2023-11-13 12:06:53 +0800

    last step table 2

--- drills.py
+++ drills.py
@@ -6,3 +6,6 @@ df = pd.read_csv("table1.csv").dropna()                                         
 df.insert(0, "ID" , [i for i in range(len(df.index))])                              #add ID
 df["Date"] = "META-" + df["Date"]                                                   #modify date
 df.insert(2, "Dummy" , [r.randint(0,1) for _ in range(len(df.index))])              #dummy
+print(df)
+
+df.to_csv("table2.csv")                                                             #last step table 2
